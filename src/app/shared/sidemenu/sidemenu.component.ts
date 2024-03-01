f10e63685c842362521ed07107431e84ce12cbef
Carlos Rodriguez Niama <clrodriguezn@gmail.com>
2024-03-01 16:29:17 -0500

    01MAR2024: Integracion rutas con el menu

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { routes } from '../../app.routes';
 
 @Component({
   selector: 'app-sidenav',
@@ -9,6 +10,25 @@ import {MatListModule} from '@angular/material/list';
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
+
+
 export class SidemenuComponent {
   shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
+
+  //  Obtenemos los items que forman parte menú
+  public menuItems = routes.map( route => route.children ?? [] )
+                            .flat()
+                            .filter( route => route && route.path )
+                            .filter( route => !route.path ?.includes(':') )
+
+
+  constructor(){
+    //  const dashboardRoutes = routes.map( route => route.children ?? [] )
+    //                              .flat()
+    //                              .filter( route => route && route.path )
+    //                              .filter( route => !route.path ?.includes(':') )
+
+    //  console.log( dashboardRoutes );
+  }
+
 }
